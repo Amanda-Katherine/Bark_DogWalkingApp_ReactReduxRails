164f3421060473150133403cf6fdb5af8059b74f
Amanda-Katherine <walker.akw@gmail.com>
2020-11-25 19:18:46 -0500

    Create seeds.rb

--- backend_api_rails/db/seeds.rb
+++ backend_api_rails/db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Walker.create(radius: 10, rating: 4.2)
+Walker.create(id: 0, radius: 0, rating: 0)
+Walker.create(radius: 20, rating: 3.83)
+Walker.create(radius: 25, rating: 4.7)
+# Walker.create(radius: , rating: )
+
+Owner.create()
+Owner.create()
+Owner.create()
+
+User.create(name: "John Waggington", address: "231 P St NW, Washington DC 20001", phone_number: "000-000-0000", username: "j_wag67", password_digest: "pass", email: "jwag@woof.com", userable_type: "Walker", userable_id: 1)
+
+User.create(name: "Sarah Barker", address: "57 Bates St St NW, Washington DC 20001", phone_number: "000-000-0000", username: "Barks_a_lot", password_digest: "pass", email: "email@woof.com", userable_type: "Walker", userable_id: 2)
+
+User.create(name: "Candace Wiggle", address: "63 Hanover Place NW, Washington DC 20001", phone_number: "000-000-0000", username: "wiggle_waggle", password_digest: "pass", email: "email@wiggle.com", userable_type: "Walker", userable_id: 3)
+
+User.create(name: "Gary Garyson", address: "700 8th St NW, Washington DC 20001", phone_number: "000-000-0000", username: "gary_the_snail", password_digest: "pass", email: "email@garyson.com", userable_type: "Owner", userable_id: 1)
+
+User.create(name: "Bert Wooferson", address: "1558 15th Place NW, Washington DC 20001", phone_number: "000-000-0000", username: "sesame_13", password_digest: "pass", email: "woof@wooferson.com", userable_type: "Owner", userable_id: 2)
+
+User.create(name: "Ernest Howler", address: "22 K St NE, Washington DC 20002", phone_number: "000-000-0000", username: "good_dog", password_digest: "pass", email: "ernie@howler.com", userable_type: "Owner", userable_id: 3)
+
+# User.create(name: "", address: "", phone_number: "000-000-0000", username: "", password_digest: "pass", email: "", userable_type: "", userable_id: )
+
+# dog behaviors: friendly, aggresive, shy, is leash trained, pulls on leash, doesn't like other people, doesn't like other dogs, jumper, chases small animals
+
+Dog.create(name: "Pepper", breed: "Heeler", age: "6 months", avatar: "blank", weight: 32, allergies: "none", behaviors: "[friendly, jumper, chases small animals]", owner_id: 2)
+
+Dog.create(name: "Austin", breed: "Black Lab", age: "12", avatar: "blank", weight: "65", allergies: "none", behaviors: "[friendly, doesn't like other dogs, is leash trained]", owner_id: 1)
+
+Dog.create(name: "River", breed: "Border Collie", age: "8", avatar: "blank", weight: "45", allergies: "none", behaviors: "[doesn't like other dogs, pees from excitement, friendly]", owner_id: 2)
+
+Dog.create(name: "Charles Barkley", breed: "Coon Hound", age: "5", avatar: "blank", weight: "60", allergies: "chicken", behaviors: "[pulls on leash, chases small animals, jumper]", owner_id: 3)
+
+Dog.create(name: "Sir Alfred Waggington", breed: "Chihuahua", age: "12", avatar: "blank", weight: "8", allergies: "lamb", behaviors: "aggressive, pulls on leash, doesn't like other dogs, doesn't like other people]", owner_id: 3)
+
+Appointment.create(date: "11/15/20", time: "5:30 PM", address: "1558 15th Place NW, Washington DC 20001", duration: "20 minutes", available: "false", walker_id: 1, dog_id: 1)
+
+Appointment.create(date: "11/19/20", time: "1 PM", address: "700 8th St NW, Washington DC 20001", duration: "15 minutes", available: "false", walker_id: 1, dog_id: 2)
+
+Appointment.create(date: "1/14/21", time: "12 PM", address: "1558 15th Place NW, Washington DC 20001", duration: "30 minutes", walker_id: 2, dog_id: 3)
+
+Appointment.create(date: "12/21/20", time: "7 AM", address: "22 K St NE, Washington DC 20002", duration: "20 minutes", walker_id: 2, dog_id: 4)
+
+Appointment.create(date: "1/12/21", time: "6 PM", address: "22 K St NE, Washington DC 20002", duration: "45 minutes", walker_id: 3, dog_id: 5)
+
+Appointment.create(date: "12/28/20", time: "12:00", address: "1558 15th Place NW, Washington DC 20001", duration: "1 hour", walker_id: 3, dog_id: 1)
+
+Appointment.create(date: "11/20/20", time: "4 PM", address: "700 8th St NW, Washington DC 20001", duration: "15 minutes", available: "false", walker_id: 1, dog_id: 2)
+
+Appointment.create(date: "12/16/20", time: "5 PM", address: "1558 15th Place NW, Washington DC 20001", duration: "30 minutes", walker_id: 3, dog_id: 3)
