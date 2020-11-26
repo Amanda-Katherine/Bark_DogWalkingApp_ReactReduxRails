cf9c72c287366587b1e9d0dd5334a4e91aec5110
Amanda-Katherine <walker.akw@gmail.com>
2020-11-25 19:17:48 -0500

    Create User Migration

---
+++ backend_api_rails/db/migrate/20201125224627_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :username
+      t.string :password_digest
+      t.string :email
+      t.references :userable, polymorphic: true, null: false
+
+      t.timestamps
+    end
+  end
+end
