14ddb12df80e0077938d0803e5d187bc5eecd3d8
Amanda-Katherine <walker.akw@gmail.com>
2020-11-25 21:06:53 -0500

    add User Serializer

--- backend_api_rails/app/serializers/owner_serializer.rb
+++ backend_api_rails/app/serializers/owner_serializer.rb
@@ -1,3 +1,9 @@
 class Api::V1::OwnerSerializer < ActiveModel::Serializer
   attributes :id
+  has_one :user, as: :userable
+  has_many :dogs
+    class Api::V1::UserSerializer < ActiveModel::Serializer
+        attributes :id, :name
+        belongs_to :userable, polymorphic: true
+    end 
 end 
--- backend_api_rails/app/serializers/walker_serializer.rb
+++ backend_api_rails/app/serializers/walker_serializer.rb
@@ -2,4 +2,8 @@ class Api::V1::WalkerSerializer < ActiveModel::Serializer
   attributes :id
   has_one :user, as: :userable
   has_many :play_dates
+    class Api::V1::UserSerializer < ActiveModel::Serializer
+        attributes :id
+        belongs_to :userable, polymorphic: true
+    end 
 end 
