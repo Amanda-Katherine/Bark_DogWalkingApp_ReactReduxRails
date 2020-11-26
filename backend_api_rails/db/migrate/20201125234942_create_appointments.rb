1c7d8aa2ca1f886827189869de601d79bed7842e
Amanda-Katherine <walker.akw@gmail.com>
2020-11-25 19:18:19 -0500

    Create Appointment Migration

---
+++ backend_api_rails/db/migrate/20201125234942_create_appointments.rb
@@ -0,0 +1,15 @@
+class CreateAppointments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :appointments do |t|
+      t.string :date
+      t.string :time
+      t.string :address
+      t.integer :duration
+      t.boolean :available, default: true
+      t.references :dog, null: false, foreign_key: true
+      t.references :walker, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
