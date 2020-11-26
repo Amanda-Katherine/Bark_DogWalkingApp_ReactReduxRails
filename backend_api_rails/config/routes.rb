2faa40e589ce3b28befbbbb3f484a98662d6743f
Amanda-Katherine <walker.akw@gmail.com>
2020-11-25 21:10:04 -0500

    create namespaced routes

--- backend_api_rails/config/routes.rb
+++ backend_api_rails/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, except: [:new, :edit]
+      resources :walkers, except: [:new, :edit]
+      resources :owners, except: [:new, :edit]
+      resources :dogs, except: [:new, :edit]
+      resources :appointments, except: [:new, :edit]
+    end
+  end
 end
