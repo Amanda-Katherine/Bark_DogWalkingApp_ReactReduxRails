c77a941cc927d7f185bec21f7795fd9505b1256a
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 02:04:19 -0500

    Build out index.js skeleton

--- frontend_react_redux/src/index.js
+++ frontend_react_redux/src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom"; //responsible for virtual DOM
-
 import App from "./App";
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import rootReducer from "./reducers/index";
+// import thunk from "redux-thunk";
+// import { Provider } from "react-redux";
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(applyMiddleware(thunk), composeWithDevTools())
+// );
 
 ReactDOM.render(
+  // <Provider store={store}>
   <App />,
-
+  // </Provider>,
   document.getElementById("root")
 );
