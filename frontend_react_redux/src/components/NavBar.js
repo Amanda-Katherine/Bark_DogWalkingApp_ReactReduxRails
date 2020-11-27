38c388e5882ffb4c3f1b66e6df24553e15370447
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 02:02:18 -0500

    begin NavBar component skeleton

---
+++ frontend_react_redux/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import UserSignup from "./UserSignup";
+
+export default function NavBar() {
+  return (
+    <>
+      <h1>hello</h1>
+      <UserSignup />
+      {/* <UserLogin/> */}
+    </>
+  );
+}
