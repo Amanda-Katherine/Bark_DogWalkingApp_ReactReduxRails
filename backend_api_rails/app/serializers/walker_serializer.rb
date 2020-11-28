692e7ef0ef81e0d5e141d443668e993f7bddcc1a
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 23:39:41 -0500

    Update walker_serializer.rb

--- backend_api_rails/app/serializers/walker_serializer.rb
+++ backend_api_rails/app/serializers/walker_serializer.rb
@@ -1,9 +1,9 @@
+# class WalkerSerializer < ActiveModel::Serializer
+#   attributes :id
+# end
+
 class Api::V1::WalkerSerializer < ActiveModel::Serializer
   attributes :id
   has_one :user, as: :userable
   has_many :play_dates
-    class Api::V1::UserSerializer < ActiveModel::Serializer
-        attributes :id
-        belongs_to :userable, polymorphic: true
-    end 
 end 
