5769302034b0457e7a4ed61beec113624cdafcc0
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 23:49:31 -0500

    add in createStore functionality

--- frontend_react_redux/src/index.js
+++ frontend_react_redux/src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom"; //responsible for virtual DOM
 import App from "./App";
-// import { createStore, applyMiddleware } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import rootReducer from "./reducers/index";
-// import thunk from "redux-thunk";
-// import { Provider } from "react-redux";
+import { compose, createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
+import { Provider } from "react-redux";
 
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(applyMiddleware(thunk), composeWithDevTools())
-// );
+const initialState = { users: [], dogs: [], appointments: [] };
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(thunk), composeWithDevTools())
+);
 
 ReactDOM.render(
   // <Provider store={store}>
