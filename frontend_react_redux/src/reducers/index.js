6d7009c105bbb2fe494774a1bc2445190e0eddcf
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 23:49:55 -0500

    add in keys for reducers

--- frontend_react_redux/src/reducers/index.js
+++ frontend_react_redux/src/reducers/index.js
@@ -4,7 +4,7 @@ import dogReducer from "./dogReducer";
 import appointmentReducer from "./appointmentReducer";
 
 export default combineReducers({
-  userReducer,
-  dogReducer,
-  appointmentReducer,
+  users: userReducer,
+  dogs: dogReducer,
+  appointments: appointmentReducer,
 });
