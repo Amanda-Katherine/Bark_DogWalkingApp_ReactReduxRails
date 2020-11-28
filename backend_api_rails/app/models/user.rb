6bda33d7bb0d3adde0ac184da641022cc34e46a1
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 23:37:46 -0500

    adds in validation and image relationship

--- backend_api_rails/app/models/user.rb
+++ backend_api_rails/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   belongs_to :userable, polymorphic: true
+  has_one_attached :image
+  validates :username, uniqueness: { case_sensitive: false }
   has_secure_password
 end
