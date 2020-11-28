5ef75f6834c30121bd9a6871b344fc17cd3cd570
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 23:37:19 -0500

    add create action and user params

--- backend_api_rails/app/controllers/api/v1/users_controller.rb
+++ backend_api_rails/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def create
+        user = User.create(user_params)
+        binding.pry
+
+        if user.valid? 
+            render json: {user: UserSerializer.new(user)}, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :gender, :email, :user_type, :address, :phone_number, :username, :password, :bio, :avatar)
+    end
 end
