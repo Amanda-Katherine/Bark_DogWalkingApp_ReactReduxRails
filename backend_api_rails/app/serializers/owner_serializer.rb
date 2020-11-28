88d3c658f5ccfc2ecf81655564a6197a48c702be
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 23:38:50 -0500

    Update owner_serializer.rb

--- backend_api_rails/app/serializers/owner_serializer.rb
+++ backend_api_rails/app/serializers/owner_serializer.rb
@@ -1,9 +1,9 @@
+# class OwnerSerializer < ActiveModel::Serializer
+#   attributes :id
+# end
+
 class Api::V1::OwnerSerializer < ActiveModel::Serializer
   attributes :id
   has_one :user, as: :userable
   has_many :dogs
-    class Api::V1::UserSerializer < ActiveModel::Serializer
-        attributes :id, :name
-        belongs_to :userable, polymorphic: true
-    end 
 end 
