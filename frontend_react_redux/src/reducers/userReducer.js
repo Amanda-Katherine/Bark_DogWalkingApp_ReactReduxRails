856c3c470b457c78482fbed8848ae199218b82b8
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 23:50:40 -0500

    create FETCH_USERS

--- frontend_react_redux/src/reducers/userReducer.js
+++ frontend_react_redux/src/reducers/userReducer.js
@@ -5,6 +5,9 @@ const initialState = {
 export const userReducer = (state = initialState, action) => {
   // debugger;
   switch (action.type) {
+    case "FETCH_USERS":
+      return { ...state, users: action.payload };
+
     case "ADD_USER":
       return console.log("add updated state info here");
 
