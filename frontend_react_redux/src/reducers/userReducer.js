546e67a031a7ad1f3f6fa03cdf7330d7d3f4bef2
Amanda-Katherine <walker.akw@gmail.com>
2020-11-27 23:50:52 -0500

    update ADD_USER reducer

--- frontend_react_redux/src/reducers/userReducer.js
+++ frontend_react_redux/src/reducers/userReducer.js
@@ -9,7 +9,7 @@ export const userReducer = (state = initialState, action) => {
       return { ...state, users: action.payload };
 
     case "ADD_USER":
-      return console.log("add updated state info here");
+      return { ...state, users: [...state.users, action.payload] };
 
     default:
       return state;
