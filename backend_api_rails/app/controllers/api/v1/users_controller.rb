10fda0388ff4278727633968ece712ca3ceb5dba
Amanda-Katherine <walker.akw@gmail.com>
2020-11-29 18:18:36 -0500

    create show action

--- backend_api_rails/app/controllers/api/v1/users_controller.rb
+++ backend_api_rails/app/controllers/api/v1/users_controller.rb
@@ -33,6 +33,10 @@ class Api::V1::UsersController < ApplicationController
         end
     end
 
+    def show 
+        user = User.find_by_id(params[:id])
+    end
+
 
     private
     def user_params
