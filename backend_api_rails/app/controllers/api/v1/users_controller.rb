f1a225d9557b70839b6defebf1e2e469a11ee7ba
Amanda-Katherine <walker.akw@gmail.com>
2020-11-29 18:18:51 -0500

    add conditional for user type to render correct data

--- backend_api_rails/app/controllers/api/v1/users_controller.rb
+++ backend_api_rails/app/controllers/api/v1/users_controller.rb
@@ -35,6 +35,22 @@ class Api::V1::UsersController < ApplicationController
 
     def show 
         user = User.find_by_id(params[:id])
+        # binding.pry
+        if user.userable_type === "Walker"
+            appointments = user.userable.appointments
+            # binding.pry
+            render json: appointments
+        elsif user.userable_type === "Owner"
+            dogs = user.userable.dogs
+            dogs_appointments = []
+
+            dogs.map do |dog| 
+                dogs_appointments.push(dog.appointments)
+            end
+            # binding.pry
+            render json: [dogs: dogs, appointments: dogs_appointments]
+            
+        end
     end
 
 
