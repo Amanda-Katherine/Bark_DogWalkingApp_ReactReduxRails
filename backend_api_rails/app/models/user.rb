f72ea02c9a158a40aa564c5163fc16cfbabb0ba1
Amanda-Katherine <walker.akw@gmail.com>
2020-11-29 00:20:16 -0500

    user signup validations

--- backend_api_rails/app/models/user.rb
+++ backend_api_rails/app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   belongs_to :userable, polymorphic: true
   has_one_attached :image
   validates :username, uniqueness: { case_sensitive: false }
+  # validates :username, :email, :name, :password, :gender, :address, :phone, :bio, presence: true
   has_secure_password
 end
