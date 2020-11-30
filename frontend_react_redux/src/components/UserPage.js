f87800aed05a5e01e3541feaf11ba46d8fd18364
Amanda-Katherine <walker.akw@gmail.com>
2020-11-30 09:24:17 -0500

    create componentDidMount and add Browser-Router component

--- frontend_react_redux/src/components/UserPage.js
+++ frontend_react_redux/src/components/UserPage.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchDogsAndAppointments } from "../actions/users";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { withRouter } from "react-router";
+// import CalendarPicker from "./CalendarPicker";
+
 class UserPage extends Component {
+  componentDidMount() {
+    console.log("fetching dogs and appointments");
+    //  ;
+    if (this.props.match) {
+      this.props.fetchDogsAndAppointments(this.props.match.url);
+    }
+  }
+
   render() {
     return (
       <div>
