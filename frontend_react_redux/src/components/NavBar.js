3530e34f1fcb954286c74c68801785cf169fa0b3
Amanda-Katherine <walker.akw@gmail.com>
2020-11-30 09:14:34 -0500

    edit NavBar Links

--- frontend_react_redux/src/components/NavBar.js
+++ frontend_react_redux/src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import UserSignup from "./UserSignup";
 
 export default function NavBar() {
   return (
     <>
-      <h1>hello</h1>
-      <UserSignup />
+      <Link to="/signup">Sign Up</Link>
+      <br />
+      {/* <Link to="/users/:id">See Your Page</Link> */}
+      {/* <Link to="/users">See all users page</Link> */}
+
       {/* <UserLogin/> */}
     </>
   );
