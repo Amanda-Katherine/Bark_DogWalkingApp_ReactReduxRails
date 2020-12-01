7e0db8842e9c66528330ed65a0c0fba485e0cf59
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 16:46:38 -0500

    add default value for walker_id

--- backend_api_rails/db/migrate/20201125234942_create_appointments.rb
+++ backend_api_rails/db/migrate/20201125234942_create_appointments.rb
@@ -7,7 +7,7 @@ class CreateAppointments < ActiveRecord::Migration[6.0]
       t.integer :duration
       t.boolean :available, default: true
       t.references :dog, null: false, foreign_key: true
-      t.references :walker, null: false, foreign_key: true
+      t.references :walker, null: false, foreign_key: true, default: 0
 
       t.timestamps
     end
