d3f232c3505d28bc6d6b2ea4161ba2d13f8d1c6e
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 16:46:21 -0500

    Delete walker_serializer.rb

--- backend_api_rails/app/serializers/walker_serializer.rb
+++
@@ -1,9 +0,0 @@
-# class WalkerSerializer < ActiveModel::Serializer
-#   attributes :id
-# end
-
-class Api::V1::WalkerSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :user, as: :userable
-  has_many :appointments
-end 
