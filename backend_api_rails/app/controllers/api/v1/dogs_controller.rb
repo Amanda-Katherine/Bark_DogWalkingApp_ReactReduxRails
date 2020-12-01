a9dfcac1d92ced1395f1f5c8cdc98ca2b3f81688
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 10:03:24 -0500

    Create dog params

--- backend_api_rails/app/controllers/api/v1/dogs_controller.rb
+++ backend_api_rails/app/controllers/api/v1/dogs_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::DogsController < ApplicationController
         dogs = Dog.all
         render json: dogs
     end
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :gender, :breed, :age, :weight, :allergies, :behaviors)
+    end
 end
