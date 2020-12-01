ebd9fb5dab2e539b8c6a0186c0dedc516ce24df3
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 10:03:54 -0500

    add create action

--- backend_api_rails/app/controllers/api/v1/dogs_controller.rb
+++ backend_api_rails/app/controllers/api/v1/dogs_controller.rb
@@ -3,6 +3,19 @@ class Api::V1::DogsController < ApplicationController
         dogs = Dog.all
         render json: dogs
     end
+
+    def create
+        dog = Dog.new(dog_params)
+        dog.owner_id = params[:ownerId]
+        binding.pry
+        if dog.valid? 
+            dog.save
+            render json: {dog: dog}
+        else
+            render json: [{ error: 'failed to create dog' }, {causation: dog.errors.full_messages}], status: :not_acceptable
+        end
+    end
+
     private
 
     def dog_params
