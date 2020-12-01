467d7a8a85a5f55719c6c48b2cd39727c5a31792
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 16:46:15 -0500

    Delete owner_serializer.rb

--- backend_api_rails/app/serializers/owner_serializer.rb
+++
@@ -1,9 +0,0 @@
-# class OwnerSerializer < ActiveModel::Serializer
-#   attributes :id
-# end
-
-class Api::V1::OwnerSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :user, as: :userable
-  has_many :dogs
-end 
