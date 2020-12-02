1696ae3c3fd75dd21d43fdc93530255ddbfe870c
Amanda-Katherine <walker.akw@gmail.com>
2020-12-02 00:03:08 -0500

    Create UsersPage.js

---
+++ frontend_react_redux/src/components/UsersPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import UserPage from "./UserPage";
+
+class UsersPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Users Page</h1>
+
+        {/* <UserPage /> */}
+      </div>
+    );
+  }
+}
+
+export default UsersPage;
