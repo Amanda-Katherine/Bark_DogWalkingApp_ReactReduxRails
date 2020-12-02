1165cc6e08647f36b8befe23d2c7d9977b928db3
Amanda-Katherine <walker.akw@gmail.com>
2020-12-02 00:06:09 -0500

    delete old reducers

--- frontend_react_redux/src/reducers/index.js
+++ frontend_react_redux/src/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
-import dogReducer from "./dogReducer";
-import appointmentReducer from "./appointmentReducer";
 
 export default combineReducers({
   users: userReducer,
-  dogs: dogReducer,
-  appointments: appointmentReducer,
 });
