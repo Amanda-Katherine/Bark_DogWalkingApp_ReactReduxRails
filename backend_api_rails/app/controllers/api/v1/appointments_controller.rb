de251976d6d0d28d3c791c7b741156f8958f5b01
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:28:20 -0500

    build create action controller

--- backend_api_rails/app/controllers/api/v1/appointments_controller.rb
+++ backend_api_rails/app/controllers/api/v1/appointments_controller.rb
@@ -3,6 +3,21 @@ class Api::V1::AppointmentsController < ApplicationController
         appointments = Appointment.all
         render json: appointments
     end
+
+    def create
+        appointment = Appointment.new(appointment_params)
+        appointment.dog_id = params[:dogId]
+        
+        if appointment.valid? 
+            appointment.save
+            render json: {appointment: appointment}
+        else
+            render json: [{ error: 'Failed to create appointment' }, {causation: appointment.errors.full_messages}], status: :not_acceptable
+        end
+    end
+
+    private
+
     def appointment_params
         params.require(:appointment).permit(:date, :time, :address, :duration, :compensation)
     end
