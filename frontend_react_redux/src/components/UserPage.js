33f2e8690e15eea2857f27233a946d07d97ebcea
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:48:44 -0500

    format what information is viewable with conditionals

--- frontend_react_redux/src/components/UserPage.js
+++ frontend_react_redux/src/components/UserPage.js
@@ -24,10 +24,75 @@ class UserPage extends Component {
   }
 
   render() {
+    let props = this.props;
     return (
       <div>
-        <h3>This is a single user page</h3>
-        {/* <CalendarPicker />; */}
+        {props ? <h3>Welcome back, {props.name}.</h3> : ""}
+        {!props.location.pathname.includes(
+          "/dogs/new" || "/appointments/new"
+        ) ? (
+          <>
+            <h3>Your Appointments</h3>
+            <AppointmentsPage
+              location={props.location}
+              appointments={props.appointments}
+              dogs={props.dogs}
+            />{" "}
+            {props.dogs ? (
+              <>
+                <h3>Your Dogs</h3> <Dogs dogs={props.dogs} />
+                <ul>
+                  <button>
+                    <Link to={`${props.match.url}/dogs/new`}>
+                      Add a new Dog
+                    </Link>
+                  </button>
+
+                  <button>
+                    <Link to={`${props.match.url}/appointments/new`}>
+                      Create new Appointment
+                    </Link>
+                  </button>
+                </ul>
+              </>
+            ) : (
+              <ul>
+                <button>
+                  <Link to={{ pathname: "/appointments", userId: props.id }}>
+                    See all appointments
+                  </Link>
+                </button>
+              </ul>
+            )}
+            <br />
+            <br />
+          </>
+        ) : null}
+
+        {props ? (
+          <Route
+            exact
+            path="/users/:id/dogs/new"
+            render={(routerProps) => (
+              <DogForm {...routerProps} dogs={props.dogs} userId={props.id} />
+            )}
+          />
+        ) : null}
+
+        {props ? (
+          <Route
+            exact
+            path="/users/:id/appointments/new"
+            render={(routerProps) => (
+              <AppointmentForm
+                {...routerProps}
+                dogs={props.dogs}
+                userId={props.id}
+                address={props.appointments[0]}
+              />
+            )}
+          />
+        ) : null}
       </div>
     );
   }
