892b03cae5b3c774a38abeed287da586d9dffe07
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:48:06 -0500

    import necessary components

--- frontend_react_redux/src/components/UserPage.js
+++ frontend_react_redux/src/components/UserPage.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchDogsAndAppointments } from "../actions/users";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { withRouter } from "react-router";
+import DogForm from "./DogForm";
+import AppointmentForm from "./AppointmentForm";
+import Dogs from "./Dogs";
+import AppointmentsPage from "./AppointmentsPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+} from "react-router-dom";
+// import { withRouter } from "react-router";
 // import CalendarPicker from "./CalendarPicker";
 
 class UserPage extends Component {
   componentDidMount() {
     console.log("fetching dogs and appointments");
-    //  ;
+
     if (this.props.match) {
       this.props.fetchDogsAndAppointments(this.props.match.url);
     }
