8e0249a298419daafe992653827601f44a1df895
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:39:17 -0500

    map each appointment to the Appointment component

--- frontend_react_redux/src/components/AppointmentsPage.js
+++ frontend_react_redux/src/components/AppointmentsPage.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import Appointment from "./Appointment";
+
 class AppointmentsPage extends Component {
+  renderAppointments = () => {
+    let appts = this.props.appointments;
+    let availableAppointments = appts.filter((apmt) => apmt.available === true);
+
+    return this.props.location.pathname.includes("/appointments")
+      ? availableAppointments.map((appt) => (
+          <Appointment key={appt.id} available={true} appointment={appt} />
+        ))
+      : appts.map((appt) => <Appointment key={appt.id} appointment={appt} />);
+  };
   render() {
     return (
       <div>
+        <>{this.props.appointments && this.renderAppointments()}</>
       </div>
     );
   }
