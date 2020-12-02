75a94c6040097a209b1aa76a5bc255c694606dac
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:28:45 -0500

    add in properties

--- backend_api_rails/app/serializers/api/v1/appointment_serializer.rb
+++ backend_api_rails/app/serializers/api/v1/appointment_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :date, :time, :address, :duration, :available
+  attributes :id, :date, :time, :address, :duration, :compensation, :available, :dogId
   belongs_to :dog
   belongs_to :walker
   has_many :owners, through: :dogs
