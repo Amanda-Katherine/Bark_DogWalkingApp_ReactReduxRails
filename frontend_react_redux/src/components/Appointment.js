4779a5367151a71b4ab3c5e27e958ad717e1f735
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:33:53 -0500

    destructure props

--- frontend_react_redux/src/components/Appointment.js
+++ frontend_react_redux/src/components/Appointment.js
@@ -1,5 +1,24 @@
 import React from "react";
 export default function Appointment(props) {
+  let {
+    id,
+    available,
+    date,
+    time,
+    address,
+    duration,
+    compensation,
+  } = props.appointment;
+
+  let {
+    name,
+    breed,
+    age,
+    weight,
+    gender,
+    behaviors,
+    allergies,
+  } = props.appointment.dog;
   return (
     <div>
     </div>
