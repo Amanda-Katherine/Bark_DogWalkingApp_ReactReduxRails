c7b3b4727e09f47712e74b05a517fc1f41054c1d
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:40:02 -0500

    Build out presentational component for a dog

--- frontend_react_redux/src/components/Dog.js
+++ frontend_react_redux/src/components/Dog.js
@@ -1,7 +1,23 @@
 import React from "react";
+
 export default function Dog(props) {
+  let dog = props.dog;
   return (
     <div>
+      <p>
+        <strong>{dog.name}</strong>
+      </p>
+      <ul>
+        <li>
+          {dog.breed} - {dog.weight}
+        </li>
+        <li>
+          {dog.gender}- {dog.age} years old
+        </li>
+        <li>Allergies: {dog.allergies}</li>
+        <li>Behaviors: {dog.behaviors}</li>
+      </ul>
+      <br />
     </div>
   );
 }
