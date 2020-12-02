b0f030c5121e790ac6bbe488dc3a3b44bbaafc6f
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:36:26 -0500

    Create appointment form

--- frontend_react_redux/src/components/AppointmentForm.js
+++ frontend_react_redux/src/components/AppointmentForm.js
@@ -18,6 +18,70 @@ class AppointmentForm extends Component {
   render() {
     return (
       <div>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Date:</label>
+          <input
+            type="text"
+            placeholder="MM/DD/YY"
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.date}
+            name="date"
+          />
+          <br />
+          <label>Time:</label>
+          <input
+            type="text"
+            placeholder="e.g. 4:30 PM"
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.time}
+            name="time"
+          />
+          <br />
+          <label>Address:</label>
+          <input
+            type="text"
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.address}
+            name="address"
+          />
+          <br />
+          <label>For which dog?</label>
+          <select
+            type="text"
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.dogId}
+            name="dogId"
+          >
+            <option value="blank"></option>
+            {this.props.dogs.map((dog, key) => {
+              return (
+                <option key={key} value={dog.id}>
+                  {dog.name}
+                </option>
+              );
+            })}
+          </select>
+          <br />
+          <label>Duration:</label>
+          <input
+            type="text"
+            placeholder="minutes"
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.duration}
+            name="duration"
+          />
+          <br />
+          <label>Compensation:</label>
+          <input
+            type="text"
+            placeholder="$ USD"
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.compensation}
+            name="compensation"
+          />
+          <br />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
