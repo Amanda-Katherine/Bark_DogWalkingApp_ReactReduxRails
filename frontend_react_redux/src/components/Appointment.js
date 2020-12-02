6078aa7ee5bb20a77ca79a42fdeda4300556d5d0
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:35:06 -0500

    build out JSX content to show an appointment

--- frontend_react_redux/src/components/Appointment.js
+++ frontend_react_redux/src/components/Appointment.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Appointment(props) {
   let {
     id,
@@ -28,6 +30,41 @@ export default function Appointment(props) {
 
   return (
     <div>
+      <span>
+        <h4>Appointment Details: {available ? "Not Booked" : "Booked"}</h4>
+        <ul>
+          <li>
+            Date: {date} at {time}
+          </li>
+          <li>Address: {address}</li>
+          <li>Requested outing length: {duration} minutes</li>
+          <li>Payment: {compensation} </li>
+        </ul>
+
+        <ul>
+          <strong> Dog: </strong>
+          <li>Name: {name}</li>
+          <li>
+            {breed} - {gender}
+          </li>
+          <li>
+            {age} years old - {weight} lbs
+          </li>
+          <li>Behaviors: {behaviors}</li>
+          <li>Allergies: {allergies}</li>
+          <button
+            value={id}
+            onClick={({ synEvent }) => {
+              handleBooking(synEvent);
+            }}
+          >
+            Accept Walk
+          </button>
+        </ul>
+      </span>
+      <br />
     </div>
   );
 }
+
+//connect acceptWalk via mapDispatchToProps
