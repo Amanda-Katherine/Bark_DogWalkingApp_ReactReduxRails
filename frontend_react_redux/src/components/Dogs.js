1dd140be443b40b3c943cff20d7329bce40dc078
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:45:06 -0500

    iterate through dogs array to map to the Dog component

--- frontend_react_redux/src/components/Dogs.js
+++ frontend_react_redux/src/components/Dogs.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import Dog from "./Dog";
+
 class Dogs extends Component {
   render() {
+    let dogs = this.props.dogs;
+    return <>{dogs && dogs.map((dog) => <Dog key={dog.id} dog={dog} />)}</>;
   }
 }
+
 export default Dogs;
