8304a1e967682ed9b49b7436605bdfce3639788f
Amanda-Katherine <walker.akw@gmail.com>
2020-12-01 23:27:57 -0500

    build out appointment params

--- backend_api_rails/app/controllers/api/v1/appointments_controller.rb
+++ backend_api_rails/app/controllers/api/v1/appointments_controller.rb
@@ -3,4 +3,7 @@ class Api::V1::AppointmentsController < ApplicationController
         appointments = Appointment.all
         render json: appointments
     end
+    def appointment_params
+        params.require(:appointment).permit(:date, :time, :address, :duration, :compensation)
+    end
 end
