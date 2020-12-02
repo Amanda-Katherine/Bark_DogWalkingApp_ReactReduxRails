bab2c8829cedc3e69cb88610e3ead411d5fbc0dd
Amanda-Katherine <walker.akw@gmail.com>
2020-12-02 03:23:28 -0500

    wrap NavBar links in header and nav tags

--- frontend_react_redux/src/components/NavBar.js
+++ frontend_react_redux/src/components/NavBar.js
@@ -5,14 +5,35 @@ import UserSignup from "./UserSignup";
 export default function NavBar() {
   return (
     <>
-      <Link to="/signup">Sign Up</Link>
-      {"        "}
-      {/* <Link to="/users/:id">See Your Page</Link> */}
-      <Link to="/appointments">See Available Walks</Link>
-      {"        "}
-      <Link to="/users/5">See Your Page</Link>
-
-      {/* <UserLogin/> */}
+      <header>
+        <nav className="nav-container, main-container">
+          <span className="logo">
+            <span className="logo-title">Bark!</span>
+            <a href="/">
+              <img
+                className="logo-icon"
+                src="styles/images/logo-icon.png"
+                // style={({ width: "30 px" }, { height: "30 px" })}
+                alt=""
+                loading="lazy"
+              />
+            </a>
+          </span>
+          {"    "}
+          <Link className="nav-link" to="/signup">
+            Sign Up
+          </Link>
+          {"        "}
+          {/* <Link to="/users/:id">See Your Page</Link> */}
+          <Link className="nav-link" to="/appointments">
+            See Available Walks
+          </Link>
+          {"        "}
+          <Link className="nav-link" to="/users/5">
+            See Your Page
+          </Link>
+        </nav>
+      </header>
     </>
   );
 }
