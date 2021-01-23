d3c5725ff7dc37de1899c7bee199534cca4d7c6e
Amanda-Katherine <walker.akw@gmail.com>
2021-01-22 23:20:45 -0500

    convert from functional component to class component

--- frontend_react_redux/src/components/Dog.js
+++ frontend_react_redux/src/components/Dog.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Dog(props) {
-  let dog = props.dog;
-  return (
-    <div>
-      <p>
-        <strong>{dog.name}</strong>
-      </p>
-      <ul>
-        <li>
-          {dog.breed} - {dog.weight}
-        </li>
-        <li>
-          {dog.gender}- {dog.age} years old
-        </li>
-        <li>Allergies: {dog.allergies}</li>
-        <li>Behaviors: {dog.behaviors}</li>
-      </ul>
-      <br />
-    </div>
-  );
+export default class Dog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      likes: 0,
+    };
+  }
+  render() {
+    let dog = this.props.dog;
+    return (
+      <div>
+        <p>
+          <strong>{dog.name}</strong>
+        </p>
+        <ul>
+          <li>
+            {dog.breed} - {dog.weight}
+          </li>
+          <li>
+            {dog.gender}- {dog.age} years old
+          </li>
+          <li>Allergies: {dog.allergies}</li>
+          <li>Behaviors: {dog.behaviors}</li>
+        </ul>
+        <br />
+      </div>
+    );
+  }
 }
